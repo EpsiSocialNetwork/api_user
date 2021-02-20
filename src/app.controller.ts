7ff4811af90ff29c3c035903220ea85475977315
Jules Peguet <julespeguet.jp@gmail.com>
2021-02-20 11:09:43 +0100

    changes before creation of docker image

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from "nest-keycloak-connect";
 
 @Controller()
 export class AppController {
   constructor() {}
 
-
+  @Get()
+  @Public()
+  home(){
+    return `Welcome to API User`;
+  }
 }
